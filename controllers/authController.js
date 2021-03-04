f94a8e97c5621abd18344db69c653f591a1e1ad1
ianalexanderz <ianstewart745@gmail.com>
2021-03-04 10:27:24 -0800

    created basic views files. added some to code to views files and authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,3 +11,19 @@ router.get('/login', (req, res) => {
 router.get('/new', (req, res) => {
   res.render('auth/new', { errors: null })
 })
+
+
+router.post('/', (req, res) => {
+  try {
+    const user = await db.user.create({
+    username: req.body.username,
+    password: req.body.password
+    })
+
+  } catch (err){
+    console.log(err)
+    res.render('auth/new', { errors: 'Error creating user, try again with a different username' })
+  }
+}) 
+
+module.exports = router
